d002ef492129b11d2e85ecb6742b538770d0bae8
Maazin_Ahmed <mahmed@ucdavis.edu>
2021-02-15 12:59:07 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!-- This file will be used to layout and connect your views -->
+<template>
+  <div id="app">
+      <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
+      <View3 />
+  </div>
+</template>
+
+<script>
+
+import View3 from './components/View3.vue'
+
+export default {
+  name: 'App',
+  components: {
+    View3
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/View3.vue
@@ -0,0 +1,492 @@
+<!-- Your HTML goes here -->
+<template>
+  <div>
+    <div class="chart" id="scatterPlot"></div>
+    <div class="chart" id="barChart"></div>
+    <div class="chart" id="lineChart"></div>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: "View3", // Feel free to rename this and the file
+  props: {
+    // Feel free to add props to communicate between components
+  },
+  async mounted() {
+    // load data
+    let mainFile = await d3.csv("/PoliceKillingsUS.csv");
+    await this.createScatterPlot(mainFile);
+    await this.createBarChart(mainFile, "16");
+    await this.createLineChart(mainFile, "16");
+  },
+  methods: {
+    createScatterPlot(mainFile) {
+      let margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      // setup x
+      let xValue = (d) => d.year;
+      let xScale = d3.scaleLinear().range([0, width]);
+      let xMap = (d) => xScale(xValue(d));
+      let xAxis = d3
+        .axisBottom()
+        .scale(xScale)
+        .tickFormat(d3.format(".0f"))
+        .ticks(3);
+
+      // setup y
+      let yValue = (d) => d.dead;
+      let yScale = d3.scaleLinear().range([height, 0]);
+      let yMap = (d) => yScale(yValue(d)); // data -> display
+      let yAxis = d3.axisLeft().scale(yScale).ticks(12);
+
+      // setup fill color
+      let cValue = (d) => d.year;
+      let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      // add the graph canvas to the body of the webpage
+      var svg = d3
+        .select("#scatterPlot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom + 50)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // aggregate the data to create a data set of year by
+      // number of cops killed.
+      let copsByDate = {};
+      for (let i = 0; i < mainFile.length; i++) {
+        let yearStr = mainFile[i]["date"].slice(-2);
+        if (copsByDate[yearStr]) {
+          copsByDate[yearStr] = copsByDate[yearStr] + 1;
+        } else {
+          copsByDate[yearStr] = 1;
+        }
+      }
+      let copsKilledData = [];
+      for (let yearAsStr in copsByDate) {
+        copsKilledData.push({
+          year: 2000 + parseInt(yearAsStr),
+          dead: copsByDate[yearAsStr],
+        });
+      }
+
+      // don't want dots overlapping axis, so add in buffer to data domain
+      xScale.domain([
+        d3.min(copsKilledData, xValue) - 1,
+        d3.max(copsKilledData, xValue) + 1,
+      ]);
+      yScale.domain([0, d3.max(copsKilledData, yValue) + 150]);
+
+      // x-axis
+      svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+      // X=axis label
+      svg
+        .append("text")
+        .attr("class", "label")
+        .attr(
+          "transform",
+          "translate(" + width / 2 + " ," + (height + margin.top + 20) + ")"
+        )
+        .text("Year");
+
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", 8)
+        .attr("class", "scattertext")
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px")
+        .style("text-decoration", "underline")
+        .text("Police Fatalities By Year");
+
+      // y-axis
+      svg.append("g").attr("class", "y axis").call(yAxis);
+
+      let currentThis = this;
+
+      // draw dots
+      svg
+        .selectAll(".dot")
+        .data(copsKilledData)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", xMap)
+        .attr("cy", yMap)
+        .on("click", function (e, d) {
+          e;
+
+          let newYear = d.year.toString().slice(-2);
+          currentThis.updateOtherCharts(mainFile, newYear);
+        })
+        .style("fill", (d) => color(cValue(d)));
+    },
+    createBarChart(mainFile, selectedYear) {
+      let margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      // set the ranges
+      let x = d3.scaleBand().range([0, width]).padding(0.1);
+      let y = d3.scaleLinear().range([height, 0]);
+
+      // setup fill color
+      let cValue = (d) => d.race;
+      let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      // add the graph canvas to the body of the webpage
+      var svg = d3
+        .select("#barChart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom + 50)
+        .append("g")
+        .attr("class", "big")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", 8)
+        .attr("class", "bartext")
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px")
+        .style("text-decoration", "underline")
+        .text("Police Fatalities By Race in Year 20" + selectedYear);
+
+      let data = this.createRaceData(mainFile, selectedYear);
+
+      let currentThis = this;
+      // Scale the range of the data in the domains
+      x.domain(data.map((d) => currentThis.updateRace(d.race)));
+      y.domain([0, d3.max(data, (d) => d.dead) + 60]);
+
+      // append the rectangles for the bar chart
+      svg
+        .selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => x(currentThis.updateRace(d.race)))
+        .attr("width", x.bandwidth())
+        .attr("y", (d) => y(d.dead))
+        .attr("height", function (d) {
+          return height - y(d.dead);
+        })
+        .style("fill", (d) => color(cValue(d)));
+
+      // add the x Axis
+      svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // add the y Axis
+      svg.append("g").attr("class", "y axis").call(d3.axisLeft(y));
+    },
+    createLineChart(mainFile, selectedYear) {
+      let margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      // add the graph canvas to the body of the webpage
+      var svg = d3
+        .select("#lineChart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom + 50)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", 8)
+        .attr("class", "linetext")
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px")
+        .style("text-decoration", "underline")
+        .text("Police Fatalities By Month in Year 20" + selectedYear);
+
+      let data = this.createMonthData(mainFile, selectedYear);
+
+      let currentThis = this;
+
+      // set the ranges
+      var x = d3
+        .scaleLinear()
+        .domain(d3.extent(data, (d) => d.month))
+        .range([0, width]);
+      svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class", "lineX")
+        .call(
+          d3.axisBottom(x).tickFormat((d) => {
+            return currentThis.updateMonth(d);
+          })
+        );
+
+      // Add Y axis
+      var y = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d.dead) + 100])
+        .range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y)).attr("class", "lineY");
+
+      let lineFunc = d3
+        .line()
+        .x((d) => x(d.month))
+        .y((d) => y(d.dead));
+      // Add the line
+      svg
+        .append("path")
+        .data([data])
+        .attr("class", "bigLine")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", lineFunc(data));
+    },
+    createRaceData(mainFile, selectedYear) {
+      // aggregate the data to create a data set of year by
+      // number of cops killed.
+      let copsByRace = {};
+      for (let i = 0; i < mainFile.length; i++) {
+        let yearStr = mainFile[i].date.slice(-2);
+        if (yearStr == selectedYear) {
+          let race = mainFile[i].race;
+          if (copsByRace[race]) copsByRace[race] = copsByRace[race] + 1;
+          else copsByRace[race] = 1;
+        }
+      }
+      let data = [];
+      for (let race in copsByRace) {
+        data.push({
+          race: race,
+          dead: copsByRace[race],
+        });
+      }
+      data.sort((a, b) => {
+        if (a.race > b.race) {
+          return 1;
+        }
+        if (a.race < b.race) {
+          return -1;
+        }
+        return 0;
+      });
+      return data;
+    },
+    updateOtherCharts(mainFile, newYear) {
+      this.updateBarChart(mainFile, newYear);
+      this.updateLineChart(mainFile, newYear);
+    },
+    updateLineChart(mainFile, newYear) {
+      let margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      let currentThis = this;
+      let data2 = currentThis.createMonthData(mainFile, newYear);
+      // set the ranges
+      var x2 = d3
+        .scaleLinear()
+        .domain(d3.extent(data2, (d) => d.month))
+        .range([0, width]);
+      var y2 = d3
+        .scaleLinear()
+        .domain([0, d3.max(data2, (d) => d.dead) + 100])
+        .range([height, 0]);
+
+      var svg2 = d3.select("#lineChart").transition();
+
+      let lineFunc = d3
+        .line()
+        .x((d) => x2(d.month))
+        .y((d) => y2(d.dead));
+
+      svg2.select(".bigLine").duration(750).attr("d", lineFunc(data2));
+
+      svg2
+        .select(".linetext")
+        .text("Police Fatalities By Month in Year 20" + newYear);
+
+      svg2
+        .select(".lineX")
+        .duration(750)
+        .call(
+          d3.axisBottom(x2).tickFormat((d) => {
+            return currentThis.updateMonth(d);
+          })
+        );
+      svg2.select(".lineY").duration(750).call(d3.axisLeft(y2));
+    },
+    updateBarChart(mainFile, newYear) {
+      let data = this.createRaceData(mainFile, newYear);
+
+      let margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      let maxVal = d3.max(data, (d) => d.dead) + 60;
+
+      let currentThis = this;
+
+      // set the ranges
+      let x4 = d3
+        .scaleBand()
+        .range([0, width])
+        .padding(0.1)
+        .domain(data.map((d) => currentThis.updateRace(d.race)));
+
+      let y4 = d3.scaleLinear().range([height, 0]).domain([0, maxVal]);
+
+      // setup fill color
+      let cValue = (d) => d.race;
+      let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+      // Select the section we want to apply our changes to
+      var svg = d3.select("#barChart");
+      svg.selectAll(".bar").remove();
+      
+      // append the rectangles for the bar chart
+      let bars = svg
+        .selectAll(".big")
+        .selectAll(".bar");
+
+      bars
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => x4(currentThis.updateRace(d.race)))
+        .attr("width", x4.bandwidth())
+        .attr("y", (d) => y4(d.dead))
+        .attr("height", function (d) {
+          return height - y4(d.dead);
+        })
+        .style("fill", (d) => color(cValue(d)));
+
+      
+
+      svg = d3.select("#barChart").transition();
+      svg.select(".x.axis")
+        .duration(750).call(d3.axisBottom(x4));
+      svg.select(".y.axis")
+        .duration(750).call(d3.axisLeft(y4));
+      svg
+        .select(".bartext")
+        .text("Police Fatalities By Race in Year 20" + newYear);
+    },
+    updateRace(race) {
+      switch (race) {
+        case "B":
+          return "Black";
+        case "W":
+          return "White";
+        case "H":
+          return "Hispanic";
+        case "A":
+          return "Asian";
+        case "O":
+          return "Other";
+        case "N":
+          return "N";
+      }
+      return "N/A";
+    },
+    createMonthData(mainFile, selectedYear) {
+      let copsByDate = {};
+      for (let i = 0; i < mainFile.length; i++) {
+        let yearStr = mainFile[i]["date"].slice(-2);
+        if (yearStr == selectedYear) {
+          let monthStr = mainFile[i]["date"].substring(3, 5);
+          if (copsByDate[monthStr]) {
+            copsByDate[monthStr] = copsByDate[monthStr] + 1;
+          } else {
+            copsByDate[monthStr] = 1;
+          }
+        }
+      }
+      let data = [];
+      let months = {};
+      for (let monthStr in copsByDate) {
+        let ele = {
+          month: parseInt(monthStr),
+          dead: copsByDate[monthStr],
+        };
+        months[monthStr] = ele;
+        data.push(ele);
+      }
+
+      return [
+        months["01"] || { month: 1, dead: 0 },
+        months["02"] || { month: 2, dead: 0 },
+        months["03"] || { month: 3, dead: 0 },
+        months["04"] || { month: 4, dead: 0 },
+        months["05"] || { month: 5, dead: 0 },
+        months["06"] || { month: 6, dead: 0 },
+        months["07"] || { month: 7, dead: 0 },
+        months["08"] || { month: 8, dead: 0 },
+        months["09"] || { month: 9, dead: 0 },
+        months["10"] || { month: 10, dead: 0 },
+        months["11"] || { month: 11, dead: 0 },
+        months["12"] || { month: 12, dead: 0 },
+      ];
+    },
+    updateMonth(month) {
+      switch (month) {
+        case 1:
+          return "Jan";
+        case 2:
+          return "Feb";
+        case 3:
+          return "Mar";
+        case 4:
+          return "Apr";
+        case 5:
+          return "May";
+        case 6:
+          return "Jun";
+        case 7:
+          return "Jul";
+        case 8:
+          return "Aug";
+        case 9:
+          return "Sep";
+        case 10:
+          return "Oct";
+        case 11:
+          return "Nov";
+        case 12:
+          return "Dec";
+      }
+      return "YES";
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.chart {
+  display: inline-block;
+  margin-right: 20px;
+}
+</style>
